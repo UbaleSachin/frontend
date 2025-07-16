44427e13ca93fbc440eeee2376bb09377766aca4
Sachin Ubale <130487678+UbaleSachin@users.noreply.github.com>
2025-07-16 10:55:21 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -407,7 +407,7 @@ class ResumeExtractor {
                 formData.append('files', file);
             });
             
-            const response = await fetch('https://resume-info-extractor.up.railway.app/extract-resume', {
+            const response = await fetch('https://python-rms.onrender.com/extract-resume', {
                 method: 'POST',
                 body: formData
             });
@@ -421,7 +421,7 @@ class ResumeExtractor {
             let extractedData = null;
             while (true) {
                 await new Promise(res => setTimeout(res, 3000)); // wait 3 seconds
-                const pollResp = await fetch(`https://resume-info-extractor.up.railway.app/extract-resume-/${jobId}`);
+                const pollResp = await fetch(`https://python-rms.onrender.com/extract-resume-/${jobId}`);
                 const pollData = await pollResp.json();
                 if (pollData.status === 'completed') {
                     extractedData = pollData;
@@ -574,7 +574,7 @@ class ResumeExtractor {
         const format = 'xlsx';
         
         try {
-            const response = await fetch('https://resume-info-extractor.up.railway.app/download-data', {
+            const response = await fetch('https://python-rms.onrender.com/download-data', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
